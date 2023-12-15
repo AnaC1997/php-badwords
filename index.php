e0d0bbe9c2825d1526852467b49510d769614f1e
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-12-15 17:39:01 +0100

    messaggio PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BADWORDS</title>
+    <?php $messaggio = "Il mio primo messaggio visualizzato tramite PHP." ?>
+</head>
+<body>
+    <h1> <?php echo $messaggio ?> </h1>
+</body>
+</html>
